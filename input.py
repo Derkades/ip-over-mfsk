9f00fab2bc51975d61e52a02159ace37694768c9
Robin <robin+git@rkslot.nl>
2022-11-18 18:34:18 +0100

    More input experiments, almost working

--- input.py
+++ input.py
@@ -1,4 +1,3 @@
-import time
 from enum import Enum
 
 import sounddevice as sd
@@ -6,11 +5,13 @@ import numpy as np
 
 import settings
 import mfsk_decode
+import tone_conversion
 
 
 class InputState(Enum):
     WAITING = 1
     RECEIVING = 2
+    IGNORE_END_TONES = 3
 
 
 input_state = InputState.WAITING
@@ -21,10 +22,10 @@ next_tone_pos = None
 tones = []
 
 
-def get_buffer_as_array_starting_from_zero():
-    copy = np.zeros(settings.RECORD_BUFFER_SIZE, dtype='i2')
-    for i in range(0, settings.RECORD_BUFFER_SIZE):
-        copy[i] = sample_buffer[(buffer_pos + i) % settings.RECORD_BUFFER_SIZE]
+def get_buffer_as_array(start, count):
+    copy = np.zeros(count, dtype='i2')
+    for i in range(0, count):
+        copy[i] = sample_buffer[(start + i) % settings.RECORD_BUFFER_SIZE]
     return copy
 
 
@@ -45,39 +46,50 @@ def callback(indata: np.ndarray, frames: int,
 def process_recording():
     global buffer_pos, input_state, next_tone_pos
     print('buffer_pos:', buffer_pos, buffer_pos % settings.RECORD_BUFFER_SIZE)
-    samples = get_buffer_as_array_starting_from_zero()
+
 
     if input_state == InputState.WAITING:
+        samples = get_buffer_as_array(buffer_pos, settings.RECORD_BUFFER_SIZE)
         first_midpoint = mfsk_decode.find_first_tone_midpoint(samples)
         if first_midpoint is not None:
             next_tone_pos = (first_midpoint + buffer_pos) % settings.RECORD_BUFFER_SIZE
             print('> found first midpoint at', first_midpoint, next_tone_pos)
+            input_state = InputState.RECEIVING
         else:
             print('> waiting for sync')
-    elif input_state == InputState.RECEIVING:
+    elif input_state == InputState.RECEIVING or input_state == InputState.IGNORE_END_TONES:
         print('receiving')
         # Check if we have received a full tone (half tone length past midpoint)
         # We may have even received multiple tones since the last time process_recording() was called
         while buffer_pos > next_tone_pos + settings.SAMPLES_PER_TONE // 2:
-            start = (next_tone_pos - settings.SAMPLES_PER_TONE // 2) % settings.RECORD_BUFFER_SIZE
-            end = (next_tone_pos + settings.SAMPLES_PER_TONE // 2) % settings.RECORD_BUFFER_SIZE
-            tone = mfsk_decode.audio_to_tone(sample_buffer[start:end])
-            print('> received tone', tone)
-            tones.append(tone)
+            start = next_tone_pos - settings.SAMPLES_PER_TONE // settings.INPUT_READ_SIZE
+            count = (settings.SAMPLES_PER_TONE // settings.INPUT_READ_SIZE) * 2
+            samples = get_buffer_as_array(start, count)
+            tone = mfsk_decode.audio_to_tone(samples)
+            if input_state == InputState.IGNORE_END_TONES:
+                if tone == settings.SYNC_END_TONE:
+                    print('... another end tone')
+                else:
+                    print('not an end tone', tone)
+                    print('transition to WAITING state')
+                    input_state = InputState.WAITING
+            else:
+                print('> received tone', tone)
+                if tone == settings.SYNC_END_TONE:
+                    print('received end tone!')
+                    input_state = InputState.IGNORE_END_TONES
+                    print(tone_conversion.tones_to_bytes(tones))
+                    break
+                elif tone < 0 or tone > 2**settings.TONE_BITS:
+                    print('illegal tone! set to 0', tone)
+                    tone = 0
+                tones.append(tone)
             next_tone_pos += settings.SAMPLES_PER_TONE
     else:
         raise ValueError(input_state)
 
 
 if __name__ == '__main__':
-    # stop_time = 0
     while True:
-        # if stop_time != 0:
-        #     print('stopped for', int((time.time_ns() - stop_time) / 1_000_000 * settings.SAMPLE_RATE), 'samples')
-        # recording = sd.rec(settings.RECORD_SIZE * settings.SAMPLE_RATE, samplerate=settings.SAMPLE_RATE, channels=1)
-        # sd.wait()
-        # stop_time = time.time_ns()
-        # recordings.append(recording)
-
         with sd.InputStream(samplerate=settings.SAMPLE_RATE, latency='high', channels=1, callback=callback):
             sd.sleep(settings.RECORD_CHUNK_SIZE * 1000)
